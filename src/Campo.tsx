f5808e1090560fc7d8589b345e9ad4fda745ee0f
JoaoPedro-Reis <joaoreis.pedro@estudante.ufjf.br>
2025-03-12 23:30:50 -0300

    alterações no css(disposição dos jogadores, bordas e background)

--- src/Campo.tsx
+++ src/Campo.tsx
@@ -1,25 +1,44 @@
-import './Campo.css'
-import Jogador from './Jogador'
+import "./Campo.css";
+import Jogador from "./Jogador";
 
 export default function Campo() {
-    return (
-    <div className='campo'>
-
-            <div className='gol'><Jogador posicao="goleiro" /></div>
-            <div className='ld'><Jogador posicao="lateral_dir" /></div>
-            <div className='zagd'><Jogador posicao="zagueiro" /></div>
-            <div className='zage'><Jogador posicao="zagueiro" /></div>
-            <div className='le'><Jogador posicao="lateral_esq" /></div>
-            <div className='vol'><Jogador posicao="meio" /></div>
-            <div className='mcd'><Jogador posicao="meio" /></div>
-            <div className='mce'><Jogador posicao="meio" /></div>
-            <div className='pd'><Jogador posicao="atacante" /></div>
-            <div className='ata'><Jogador posicao="atacante" /></div>
-            <div className='pe'><Jogador posicao="atacante" /></div>
-
+  return (
+    <div className="campo">
+      <div className="gol">
+        <Jogador posicao="goleiro" />
+      </div>
+      <div className="ld">
+        <Jogador posicao="lateral_dir" />
+      </div>
+      <div className="zagd">
+        <Jogador posicao="zagueiro" />
+      </div>
+      <div className="zage">
+        <Jogador posicao="zagueiro" />
+      </div>
+      <div className="le">
+        <Jogador posicao="lateral_esq" />
+      </div>
+      <div className="vol">
+        <Jogador posicao="meio" />
+      </div>
+      <div className="mcd">
+        <Jogador posicao="meio" />
+      </div>
+      <div className="mce">
+        <Jogador posicao="meio" />
+      </div>
+      <div className="pd">
+        <Jogador posicao="atacante" />
+      </div>
+      <div className="ata">
+        <Jogador posicao="atacante" />
+      </div>
+      <div className="pe">
+        <Jogador posicao="atacante" />
+      </div>
     </div>
-    );
+  );
 }
-
 
 //
--- src/Jogador.tsx
+++ src/Jogador.tsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
-import data from './jogadores.json';
-import './Jogador.css';
+import React, { useState } from "react";
+import data from "./jogadores.json";
+import "./Jogador.css";
 
 interface Player {
   nome: string;
@@ -17,31 +17,33 @@ interface Positions {
 
 export default function Jogador({ posicao }: JogadorProps) {
   const [selectedPlayer, setSelectedPlayer] = useState<Player>({
-    nome: 'Vazio',
-    card: 'https://futcardsfifa.com/app/uploads/2024/09/FC25-Gold.png',
+    nome: "Vazio",
+    card: "https://futcardsfifa.com/app/uploads/2024/09/FC25-Gold.png",
   });
 
   const positions: Positions = data.posicoes;
 
   function handlePlayerChange(event: React.ChangeEvent<HTMLSelectElement>) {
     const playerName = event.target.value;
-    const player = positions[posicao].find((p: Player) => p.nome === playerName) || {
-      nome: 'Vazio',
-      card: 'https://futcardsfifa.com/app/uploads/2024/09/FC25-Gold.png',
+    const player = positions[posicao].find(
+      (p: Player) => p.nome === playerName
+    ) || {
+      nome: "Vazio",
+      card: "https://futcardsfifa.com/app/uploads/2024/09/FC25-Gold.png",
     };
     setSelectedPlayer(player);
   }
 
   return (
     <div className="jogador-container">
-
-      <img
-        className="jogador-imagem"
-        src={selectedPlayer.card}
-      />
+      <img className="jogador-imagem" src={selectedPlayer.card} />
 
       <div className="seletor-container">
-        <select id="player" value={selectedPlayer.nome} onChange={handlePlayerChange}>
+        <select
+          id="player"
+          value={selectedPlayer.nome}
+          onChange={handlePlayerChange}
+        >
           {positions[posicao].map((player: Player) => (
             <option key={player.nome} value={player.nome}>
               {player.nome}
